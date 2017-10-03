d4873e63ef5b6d64b30a7f96dfa41e7adc0e78ff
Mali Michael <malimccalla@gmail.com>
2017-10-03 17:16:38 +0100

    Add renderButtons functions to determine if there is a user

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,9 +3,24 @@ import { graphql } from 'react-apollo';
 import currentUserQuery from '../queries/currentUser';
 
 class Header extends Component {
+  renderButtons() {
+    const { loading, currentUser } = this.props.data;
+    console.log('user', this.props.data);
+    if (loading) return <div />;
+
+    if (currentUser) {
+      return <div>Logout</div>;
+    } else {
+      return <div>Please sign in</div>;
+    }
+  }
+
   render() {
-    console.log(this.props.data);
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">{this.renderButtons()}</div>
+      </nav>
+    );
   }
 }
 
