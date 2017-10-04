d8ca986457910c55f01ba9ee96983f160812847f
Mali Michael <malimccalla@gmail.com>
2017-10-04 18:03:11 +0100

    Refetch user data on signup

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import AuthForm from './AuthForm';
 import signupMutation from '../mutations/signup';
+import query from '../queries/currentUser';
 
 class SignupForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { errors: [] };
+  }
+
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password }
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }]
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(err => err.message);
+        this.setState({ errors });
+      });
   }
 
   render() {
     return (
       <div>
-        <AuthForm onSubmit={this.onSubmit.bind(this)} errors={[]} />
+        <AuthForm
+          onSubmit={this.onSubmit.bind(this)}
+          errors={this.state.errors}
+        />
       </div>
     );
   }
